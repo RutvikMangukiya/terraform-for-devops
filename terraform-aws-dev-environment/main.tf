09a416d16e41902a527fe3604d8ffd503aae070a
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-04-25 16:23:28 +0530

    Add aws resource: vpc

---
+++ terraform-aws-dev-environment/main.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "rut_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "Name" = "dev"
+  }
+}
+
+resource "aws_subnet" "rut_public_subnet" {
+  vpc_id                  = aws_vpc.rut_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "rut_internet_gateway" {
+  vpc_id = aws_vpc.rut_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "rut_public_rt" {
+  vpc_id = aws_vpc.rut_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.rut_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.rut_internet_gateway.id
+}
+
+resource "aws_route_table_association" "rut_public_assoc" {
+  subnet_id      = aws_subnet.rut_public_subnet.id
+  route_table_id = aws_route_table.rut_public_rt.id
+}
