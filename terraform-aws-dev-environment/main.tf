125ebe0ff3cb00dbb78be34e5cc47e54782bb3e1
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-04-25 18:05:03 +0530

    Add aws resource: EC2

--- terraform-aws-dev-environment/main.tf
+++ terraform-aws-dev-environment/main.tf
@@ -70,3 +70,21 @@ resource "aws_key_pair" "rut_auth" {
   key_name   = "rutkey"
   public_key = file("~/.ssh/rutkey.pub")
 }
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.rut_auth.id
+  vpc_security_group_ids = [aws_security_group.rut_sg.id]
+  subnet_id              = aws_subnet.rut_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "dev_node"
+  }
+}
