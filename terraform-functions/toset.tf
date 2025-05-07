491e4a58d1c2019550d2ba92676f1bbbccfeab6e
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-05-07 13:41:21 +0530

    Added toset function

---
+++ terraform-functions/toset.tf
@@ -0,0 +1,14 @@
+# The toset function is used to convert a list or tuple into a set. This transformation helps eliminate duplicate values, ensuring that each element in the set is unique.
+
+# Syntax:
+# toset(list)
+
+variable "my_list" {
+  default = ["apple", "banana", "apple", "orange"]
+}
+
+output "unique_fruits" {
+  value = toset(var.my_list)
+}
+
+# In this example, the toset function converts the list ["apple", "banana", "apple", "orange"] into a set, resulting in {"apple", "banana", "orange"} by removing the duplicate "apple." This is particularly useful when you want to ensure unique values in your configurations.
