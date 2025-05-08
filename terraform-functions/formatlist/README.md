0d10a07f11f770a1a13166c93ab1faba054276e6
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-05-08 14:41:12 +0530

    Added formatlist function

---
+++ terraform-functions/formatlist/README.md
@@ -0,0 +1,11 @@
+## formatlist Function:
+- The `formatlist` function formats a list of values using a specified pattern.
+- It’s perfect for situations where you need to apply consistent formatting to multiple elements.
+
+## Synatx:
+- `formatlist(format_string, values...)`
+- `format_string`: The format pattern to apply to each value in thr list.
+- `values...`: The list of values to format.
+
+## Supported Placeholders:
+- The placefolders in `formatlist` work the same way as those in the `format` function, allowing you to handle strings, integers, floats, and booleans.
---
+++ terraform-functions/formatlist/formatlist.tf
@@ -0,0 +1,47 @@
+# 1. Formatting a List of Strings
+
+variable "cities" {
+    default = ["New York", "Los Angles", "Chicago"]
+}
+
+output "city_greetings" {
+    value = formatlist("Welcome to %s!", var.cities)
+}
+
+# Output: ["Welcome to New York!", "Welcome to Los Angeles!", "Welcome to Chicago!"]
+
+# 2. Formatting a List on Integers 
+
+variable "scores" {
+    default = [95, 85, 75]
+}
+
+output "formatted_scores" {
+    value = formatlist("Score: %d", var.scores)
+}
+
+# Output: ["Score: 95", "Score: 85", "Score: 75"]
+
+# 3. Formatting a List of Floats
+
+variable "percentages" {
+  default = [0.875, 0.923, 0.658]
+}
+
+output "formatted_percentages" {
+  value = formatlist("Percentage: %.1f%%", var.percentages)
+}
+
+# Output: ["Percentage: 87.5%", "Percentage: 92.3%", "Percentage: 65.8%"]
+
+# 4. Using Automatic Formatting with %v in Lists
+
+variable "mixed_values" {
+  default = [42, "banana", true]
+}
+
+output "formatted_list" {
+  value = formatlist("Value: %v", var.mixed_values)
+}
+
+# Output: ["Value: 42", "Value: banana", "Value: true"]
