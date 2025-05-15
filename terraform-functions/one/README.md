e584ab7a3c1f5b8bc76184d69f710c0a35fc9fd2
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-05-15 18:48:09 +0530

    Added one function

---
+++ terraform-functions/one/README.md
@@ -0,0 +1,7 @@
+## one Function:
+- The one function checks if exactly one of its arguments is non-null and returns that value. 
+- If none or more than one argument is non-null, it results in an error.
+
+## Syntax:
+- `one(value1, value2, ...)`
+- `value1, value2, ...`: A list of values to evaluate. It checks if exactly one is non-null.
---
+++ terraform-functions/one/one.tf
@@ -0,0 +1,38 @@
+# 1. Basic One Usage
+
+variable "single_value" {
+  default = "only_value"
+}
+
+output "exactly_one_non_null" {
+  value = one([var.single_value, null])
+}
+
+# Output: "only_value"
+
+# The function returns single_value because it is the only non-null value.
+#-------------------------------------------------------------------------------
+
+# 2. More than One Non-Null
+
+variable "var4" {
+    default = "value_1"
+}
+
+variable "var5" {
+    default = "value_2"
+}
+
+output "more_than_one_non_null" {
+    value = one(var.var4, var.var5)
+}
+
+# Error: The function will throw an error because there are two non-null values.
+#-------------------------------------------------------------------------------
+
+# 3. No Non-Null Values
+output "no_non_null" {
+  value = one(null, null)
+}
+
+# Error: The function will throw an error because there are no non-null values.
